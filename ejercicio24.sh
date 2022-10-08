be997d1c061cb00f047d85cfd9e222c81d1ac3b8
LucianoGimenezz <luciano.gimenezz21@gmail.com>
2022-10-08 17:11:58 -0300

    :memo: [ADD] Ejercicio 24

---
+++ ejercicio24.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+vector1=(1 80 65 35 2)
+vector2=(5 98 3 41 8)
+
+for((i=0; i < ${#vector1[@]}; i++))
+do
+  echo "La suma entre los elementos de la posicion $i de los vectores es:  `expr ${vector1[$i]} + ${vector2[$i]}`"
+done
